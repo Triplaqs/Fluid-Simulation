63324e9fac9c75ba16f20e4b4f017c8d2679072c
Axel Eration <pereyrol.axel@gmail.com>
2026-03-12 14:03:15 +0100

    ajout apps screens + lib serial pour Bluetooth

--- src/interaction.cpp
+++ src/interaction.cpp
@@ -9,3 +9,31 @@
 //headers
 #include "utils.h"
 #include "interaction.h"
+//Pour le Bluetooth
+#include <iostream>
+#include <string>
+// Utilise une lib simple comme "serialib.h" pour Windows/Linux
+#include "serialib.h" 
+
+serial serialPort;
+float fluidDensity = 1.0f;
+float fluidDirection = 0.0f;
+
+void updateBluetooth() {
+    char buffer[32];
+    // On lit si quelque chose arrive sur le port COM (ex: "COM3")
+    if (serialPort.readString(buffer, '\n', 32, 0) > 0) {
+        std::string msg(buffer);
+        
+        // On cherche le séparateur
+        size_t sep = msg.find(":");
+        if (sep != std::string::npos) {
+            std::string id = msg.substr(0, sep);
+            float value = std::stof(msg.substr(sep + 1));
+
+            // On met à jour les variables de la simulation
+            if (id == "01") fluidDensity = value;
+            else if (id == "02") fluidDirection = value;
+        }
+    }
+}
--- src/interaction.h
+++ src/interaction.h
@@ -1,5 +1,11 @@
 #ifndef INTERACTION_H
 #define INTERACTION_H
 
+//On associe chaque paramètre à un numéro !
+
+/*
+01 : Densité
+02 : Direction
+*/
 
 #endif 
